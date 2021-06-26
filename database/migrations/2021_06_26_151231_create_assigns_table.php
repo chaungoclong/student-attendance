b962a132cc0431ae32ca08aa3ca7938719f4f46a
long <chaungoclong2411@gmail.com>
2021-06-26 23:21:10 +0700

    update migration: remove model Teacher and add some columns into migration of User Model

--- database/migrations/2021_06_26_151231_create_assigns_table.php
+++ database/migrations/2021_06_26_151231_create_assigns_table.php
@@ -17,13 +17,13 @@ class CreateAssignsTable extends Migration
             $table->id();
             $table->unsignedBigInteger("id_grade");
             $table->unsignedBigInteger("id_subject");
-            $table->unsignedBigInteger("id_teacher");
+            $table->unsignedBigInteger("id_user");
             $table->float("time_done")->default(0);
             $table->timestamps();
-            $table->unique(["id_grade", "id_subject", "id_teacher"]);
+            $table->unique(["id_grade", "id_subject", "id_user"]);
             $table->foreign("id_grade")->references("id")->on("grades");
             $table->foreign("id_subject")->references("id")->on("subjects");
-            $table->foreign("id_teacher")->references("id")->on("teachers");
+            $table->foreign("id_user")->references("id")->on("users");
         });
     }
 
